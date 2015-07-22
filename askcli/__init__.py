15da1afef5de52388d1b1aaf62b65836654fe4f3
Cedric Thivolle <cedric.thivolle@voxity.fr>
2015-07-22 15:10:57 +0200

    egmentation code + amélioration du package

---
+++ askcli/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import, division, print_function, unicode_literals
+from askcli.menu import Menu
+from askcli.item import Item
+"""
+    askcli
+    ------
+    
+    Modul to simplify your menu choice in yours python scripts
+
+    like : 
+      1 - action a
+      2 - action b
+      q - exit
+    ? enter your choice : 
+"""
+
+__author__ = "THIVOLLE-CAZAT cédric"
+__version__ = '0.3.0-b'
+__maintainer__ = "THIVOLLE-CAZAT cédric"
+__status__ = "dev"
+
+Item = Item
+Menu = Menu
---
+++ askcli/item.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+class Item(object):
+
+    key = ''
+    txt = ''
+    activate = False
+
+    def __init__(self, key, txt, activate=False):
+        """
+        :param str key: clef de l'item.
+        :param str txt: text de l'item
+        """
+        self.key = str(key)
+        self.txt = str(txt)
+        self.activate = bool(activate)
+
+    def __repr__(self):
+        """
+        :rtype: str
+        :return: representation de l'objet
+        """
+        return "<menu.Item : {0} - {1}>".format(self.key, self.txt)
+
+    def __str__(self):
+        """
+        :rtype: str
+        :return: représentaion to string de l'objet
+        """
+
+        ret = "{0} - {1}".format(self.key, self.txt)
+        if (self.activate):
+            return ">{0}".format(ret)
+        else:
+            return " {0}".format(ret)
+
+    def __eq__(self, other):
+        """
+        :param all other: objet à tester
+
+        :return: si les objet sont égale. c'est à dire que les key sont égale
+        :rtype: bool
+        """
+
+
+        try:
+            return (str(self.key) == str(other.key))
+        except Exception:
+            return False
