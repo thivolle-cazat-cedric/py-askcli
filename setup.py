7ff8f87641883b6e5326b52df24a73a8025952d3
thivolle cedric <cedric@thivolle-cazat.fr>
2015-08-05 21:02:56 +0200

    for pipy

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+setup(
+  name = 'askcli',
+  packages = ['askcli'], # this must be the same as the name above
+  version = '1.0.0',
+  description = 'generate interactive menus in a python script.',
+  author = 'THIVOLLE-CAZAT Cédric',
+  author_email = '',
+  url = 'https://github.com/thivolle-cazat-cedric/py-askcli.git',
+  keywords = ['scripting', 'menu', 'yes/no'], # arbitrary keywords
+  classifiers = [],
+)
